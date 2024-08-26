9565392d7958f70388811bf2b343d17305bb46d4
venomous-maker <morganokumu8+1@gmail.com>
2024-08-26 16:07:57 +0300

    Added system settings service provider

--- src/Providers/SystemSettingsServiceProvider.php
+++ src/Providers/SystemSettingsServiceProvider.php
@@ -1,8 +1,26 @@
 <?php
 
 namespace Venom\SystemSettings\Providers;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class SystemSettingsServiceProvider
+class SystemSettingsServiceProvider extends PackageServiceProvider
 {
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('system-settings-package')
+            ->hasConfigFile('system_settings')
+            ->hasViews()
+            ->hasMigration('create_system_settings_table')
+            ->hasCommands([
+                SyncSettingsCommand::class,
+                ClearSettingsCacheCommand::class,
+            ]);
+    }
 
+    public function bootingPackage()
+    {
+        // Register additional services, middleware, or events here.
+    }
 }
