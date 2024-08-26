c01b2600e4eeadd0a5524c3bbcc12b5efe4cc509
venomous-maker <morganokumu8+1@gmail.com>
2024-08-26 16:07:29 +0300

    Added system settings model

---
+++ src/Models/SystemSettings.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Venom\SystemSettings\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SystemSettings extends Model
+{
+    protected $fillable = ['key', 'type', 'value'];
+
+    // Accessor for converting JSON strings to arrays
+    public function getValueAttribute($value)
+    {
+        return $this->type === 'json' ? json_decode($value, true) : $value;
+    }
+
+    // Mutator for converting arrays to JSON strings
+    public function setValueAttribute($value)
+    {
+        $this->attributes['value'] = $this->type === 'json' ? json_encode($value) : $value;
+    }
+
+    public function valueExists($key){
+        return $this->where('key', $key)->count() > 0;
+    }
+}
