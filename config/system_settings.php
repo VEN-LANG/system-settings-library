a13e22b3c54cd917a6ce741312716871fb739791
venomous-maker <morganokumu8+1@gmail.com>
2024-08-27 11:36:56 +0300

    Made model dynamic in system settings

--- config/system_settings.php
+++ config/system_settings.php
@@ -3,4 +3,5 @@
 return array(
     'cache_duration' => 60, // Cache duration for settings in minutes
     'allowed_types' => ['string', 'integer', 'boolean', 'json'],
+    'model' => \Venom\SystemSettings\Models\SystemSettings::class,
 );
