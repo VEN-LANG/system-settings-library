fd0bbf5cb3c3568f9e18ea51a523a12b77d2723e
venomous-maker <morganokumu8+1@gmail.com>
2025-01-29 15:40:12 +0300

    Added more conf variables

--- src/Models/SystemSettings.php
+++ src/Models/SystemSettings.php
@@ -4,94 +4,94 @@ namespace Venom\SystemSettings\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Crypt;
 
 class SystemSettings extends Model
 {
     protected $fillable = ['key', 'type', 'value'];
-
-    public static string $cachename = 'system_settings';
-
     protected $table = 'venom_system_settings';
 
-    // Accessor for converting JSON strings to arrays
+    protected static array $encryptedKeys = [];
+    protected static string $cachename;
+
+    protected static function boot()
+    {
+        parent::boot();
+        self::$cachename = config('system_settings.cache_key_prefix', 'system_settings');
+
+        static::saved(fn ($model) => self::refreshCache($model));
+        static::deleted(fn ($model) => Cache::forget(self::$cachename.".{$model->key}"));
+    }
+
     public function getValueAttribute($value)
     {
-        return $this->type === 'json' ? json_decode($value, true) : $value;
+        return $this->decryptValue($value);
     }
 
-    // Mutator for converting arrays to JSON strings
     public function setValueAttribute($value)
     {
-        $this->attributes['value'] = $this->type === 'json' ? json_encode($value) : $value;
+        $this->attributes['value'] = $this->encryptValue($value);
     }
 
-    // Check if a specific key exists
-    public function hasKey($key)
+    public static function getValueByKey($key, $default = null)
     {
-        return self::where('key', $key)->exists();
-    }
-
-    // Check if a specific type exists
-    public function hasType($type)
-    {
-        return self::where('type', $type)->exists();
-    }
-
-    // Retrieve settings by type
-    public function withType($type)
-    {
-        return self::where('type', $type)->get();
-    }
-
-    // Retrieve setting value by key
-    public static function getValueByKey($key)
-    {
-        $setting = Cache::remember(self::$cachename.".$key", 60, function() use ($key) {
-            return self::where('key', $key)->first();
+        return Cache::remember(self::$cachename.".$key", config('system_settings.cache_duration', 60), function () use ($key, $default) {
+            return optional(self::where('key', $key)->first())->value ?? $default;
         });
-
-        return $setting ? $setting->value : null;
     }
 
-    // Update setting by key
-    public static function setValueByKey($key, $value)
+    public static function setValueByKey($key, $value, $type = 'string')
     {
-        $setting = self::updateOrCreate(['key' => $key], ['value' => $value]);
-        Cache::forget(self::$cachename.".$key");
-
+        $setting = self::updateOrCreate(['key' => $key], ['value' => $value, 'type' => $type]);
+        self::refreshCache($setting);
         return $setting;
     }
 
-    // Remove setting by key
     public static function removeByKey($key)
     {
-        $setting = self::where('key', $key)->first();
-
-        if ($setting) {
-            Cache::forget(self::$cachename.".$key");
-            return $setting->delete();
-        }
-
-        return false;
+        return optional(self::where('key', $key)->first()?->delete());
     }
 
-    // Scope to retrieve settings by a partial key match
     public function scopeByPartialKey($query, $partialKey)
     {
         return $query->where('key', 'like', "%$partialKey%");
     }
 
-    // Refresh cache after saving
-    protected static function boot()
+    protected function decryptValue($value)
     {
-        parent::boot();
+        return in_array($this->key, self::$encryptedKeys) ? Crypt::decryptString($value) : $this->formatValue($value, $this->type);
+    }
 
-        static::saved(function ($model) {
-            Cache::forget(self::$cachename.".{$model->key}");
-        });
+    protected function encryptValue($value)
+    {
+        return in_array($this->key, self::$encryptedKeys) ? Crypt::encryptString($value) : $this->sanitizeValue($value, $this->type);
+    }
 
-        static::deleted(function ($model) {
-            Cache::forget(self::$cachename.".{$model->key}");
-        });
+    protected function formatValue($value, $type)
+    {
+        return match ($type) {
+            'integer' => (int) $value,
+            'boolean' => filter_var($value, FILTER_VALIDATE_BOOLEAN),
+            'json', 'array' => json_decode($value, true, 512, JSON_THROW_ON_ERROR),
+            'float' => (float) $value,
+            default => (string) $value,
+        };
+    }
+
+    protected function sanitizeValue($value, $type)
+    {
+        return match ($type) {
+            'integer' => (int) $value,
+            'boolean' => (bool) $value,
+            'json', 'array' => json_encode($value, JSON_THROW_ON_ERROR),
+            'float' => (float) $value,
+            default => (string) $value,
+        };
+    }
+
+    protected static function refreshCache($model)
+    {
+        Cache::forget(self::$cachename.".{$model->key}");
+        Cache::put(self::$cachename.".{$model->key}", $model->value, config('system_settings.cache_duration', 60));
     }
 }
